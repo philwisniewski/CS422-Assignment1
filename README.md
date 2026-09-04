25e4e8868fd992a67078aa63c2045389b2c8bd7f
Philip Wisniewski <pwisnie@purdue.edu>
2026-09-04 11:54:14 -0400

    Init

---
+++ README.md
@@ -0,0 +1,8 @@
+# CS422 Assignment 1
+
+### Group Members:
+ - Richie Tan
+ - Daniel Chindris
+ - Emma Zheng
+ - Saverio Miller
+ - Philip Wisniewski
---
+++ assignment.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+FILE_PATH = "listed_iperf3_servers.csv"
+
+
+def part_1(df):
+    return
+
+def part_2(df):
+    return
+
+if __name__ == "__main__":
+    df = pd.read_csv(FILE_PATH)
+    print(df.head())
+
+    part_1(df)
+    part_2(df)
