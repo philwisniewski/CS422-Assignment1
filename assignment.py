78eda825c75ae29263b48ab68431c3ddd5a94f3a
Philip Wisniewski <pwisnie@purdue.edu>
2026-09-04 12:25:42 -0400

    typo fixes

--- assignment.py
+++ assignment.py
@@ -9,21 +9,35 @@ def part_1(df):
     return
 
 def part_2(df):
+    mapping = {}
+
     for idx, row in df.head(5).iterrows():
+        print("running traceroute for ", row["IP/HOST"])
+
         traceroute_result = subprocess.run([
                 "traceroute", "-n", "-q", "1", row["IP/HOST"],
             ], capture_output=True, text=True)
-        lines = result.stdout.splitlines()
+        lines = traceroute_result.stdout.splitlines()
 
-        num_hops = lines[-1].split(" ")[0]
-        final_latency = float(lines[-1].split(" ")[-2])
-        
+        # for part (b)
+        latencies = []
+        # for part (c)
+        num_hops = lines[-1].split()[0]
+        final_latency = float(lines[-1].split()[-2])
+
         for line in lines:
-            splt = line.split(" ")
+            splt = line.split()
             if len(splt) == 4:
                 # valid line
                 latencies.append(float(splt[-2]))
         
+        # for part (b)
+        per_hop_latencies = [latencies[i] - latencies[i - 1] for i in range(1, len(latencies))]
+
+        mapping[row["IP/HOST"]] = {}
+        mapping[row["IP/HOST"]]["per_hop_latencies"] = per_hop_latencies
+        mapping[row["IP/HOST"]]["num_hops"] = num_hops
+        mapping[row["IP/HOST"]]["final_latency"] = final_latency        
 
     return
 
